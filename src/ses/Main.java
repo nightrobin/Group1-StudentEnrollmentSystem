9358fa3fa502291f02831e9a3dfeeace392c7077
cypherhan <70126136+cypherhan@users.noreply.github.com>
2021-06-06 00:22:36 +0800

    Update Main.java
    added java swing

--- src/ses/Main.java
+++ src/ses/Main.java
@@ -6,21 +6,69 @@ public class Main {
 
     public static void main(String[] args){
 
+        // Java Swing
+
         JFrame f=new JFrame();//creating instance of JFrame
 
-        final JTextField tf=new JTextField();
-        tf.setBounds(50,50, 150,20);
+        // labels
+        JLabel l,l1,l2,l3,l4,l5,l6,l7,l8;
+        l=new JLabel("Student Information");
+        l.setBounds(35,40, 200,30);
+        l1=new JLabel("Student Number:");
+        l1.setBounds(60,90, 100,30);
+        l2=new JLabel("First Name:");
+        l2.setBounds(60,130, 100,30);
+        l3=new JLabel("Middle Name:");
+        l3.setBounds(60,170, 100,30);
+        l4=new JLabel("Last Name:");
+        l4.setBounds(60,210, 100,30);
+        l5=new JLabel("Address:");
+        l5.setBounds(60,250, 100,30);
+        l6=new JLabel("Father's Name:");
+        l6.setBounds(60,290, 100,30);
+        l7=new JLabel("Mother's Name:");
+        l7.setBounds(60,330, 100,30);
+        l8=new JLabel("GWA:");
+        l8.setBounds(60,370, 100,30);
+        f.add(l); f.add(l1); f.add(l2); f.add(l3); f.add(l4); f.add(l5); f.add(l6); f.add(l7); f.add(l8);
 
-        JButton b=new JButton("click");//creating instance of JButton
-        b.setBounds(130,100,100, 40);//x axis, y axis, width, height
+        // text fields
+        JTextField t1,t2,t3,t4,t5,t6,t7,t8;
+        t1=new JTextField("Enter student number here...");
+        t1.setBounds(180,90, 200,30);
+        t2=new JTextField("Enter first name here...");
+        t2.setBounds(180,130, 200,30);
+        t3=new JTextField("Enter middle name here...");
+        t3.setBounds(180,170, 200,30);
+        t4=new JTextField("Enter last name here...");
+        t4.setBounds(180,210, 200,30);
+        t5=new JTextField("Enter address here...");
+        t5.setBounds(180,250, 200,30);
+        t6=new JTextField("Enter father's name here...");
+        t6.setBounds(180,290, 200,30);
+        t7=new JTextField("Enter mother's name here...");
+        t7.setBounds(180,330, 200,30);
+        t8=new JTextField("Enter GWA here...");
+        t8.setBounds(180,370, 200,30);
+        f.add(t1); f.add(t2); f.add(t3); f.add(t4); f.add(t5); f.add(t6); f.add(t7); f.add(t8);
 
-        tf.setText("Welcome to Javatpoint.");
-
+        // button
+        JButton b=new JButton("Submit");//creating instance of JButton
+        b.setBounds(280,430,100, 40);//x axis, y axis, width, height
         f.add(b);//adding button in JFrame
 
-        f.setSize(400,500);//400 width and 500 height
+        f.setSize(440,580);//400 width and 500 height
         f.setLayout(null);//using no layout managers
         f.setVisible(true);//making the frame visible
+
+
+        // Main code
+
+        String studentNo;
+        Scanner sNo = new Scanner(System.in);
+        System.out.println("Student number...");
+        studentNo = sNo.nextLine();
+        System.out.println("====================================");
 
         String firstName;
         Scanner fName =  new Scanner(System.in);
@@ -28,18 +76,16 @@ public class Main {
         firstName = fName.nextLine();
         System.out.println("====================================");
 
+        String middleName;
+        Scanner mName =  new Scanner(System.in);
+        System.out.println("Enter Middle Name here...");
+        middleName = mName.nextLine();
+        System.out.println("====================================");
 
         String lastName;
         Scanner lName =  new Scanner(System.in);
         System.out.println("Enter Last Name here...");
         lastName = lName.nextLine();
-        System.out.println("====================================");
-
-
-        String middleName;
-        Scanner mName =  new Scanner(System.in);
-        System.out.println("Enter Middle Name here...");
-        middleName = mName.nextLine();
         System.out.println("====================================");
 
         String address;
@@ -58,12 +104,6 @@ public class Main {
         Scanner nOM = new Scanner(System.in);
         System.out.println("Enter your Mother's name here...");
         nameOfMother = nOM.nextLine();
-        System.out.println("====================================");
-
-        String studentNo;
-        Scanner sNo = new Scanner(System.in);
-        System.out.println("Student number...");
-        studentNo = sNo.nextLine();
         System.out.println("====================================");
 
         int grade;
