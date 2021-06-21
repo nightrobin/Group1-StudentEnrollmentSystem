81f592de26345a3f3610b171fa8ea6a438c9b41e
arianneconde <78188638+arianneconde@users.noreply.github.com>
2021-06-22 00:04:33 +0800

    Update Fee.java
    rar

--- src/Main/Fee.java
+++ src/Main/Fee.java
@@ -14,6 +14,7 @@ import java.awt.print.*;
 import javafx.print.Printer;
 import java.io.*;
 import java.io.IOException;
+import java.awt.Color;
 
 // Creating the fee class
 public class Fee extends JFrame {
